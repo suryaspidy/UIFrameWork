c25a7df1f0e6bc767971114cb6923ba9bc319c61
suryaspidy <ss0972203@gmail.com>
2021-07-22 15:07:39 +0530

    Initial Commit

---
+++ UIFrameWork/UIFrameWork.h
@@ -0,0 +1,18 @@
+//
+//  UIFrameWork.h
+//  UIFrameWork
+//
+//  Created by surya-zstk231 on 22/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIFrameWork.
+FOUNDATION_EXPORT double UIFrameWorkVersionNumber;
+
+//! Project version string for UIFrameWork.
+FOUNDATION_EXPORT const unsigned char UIFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIFrameWork/PublicHeader.h>
+
+
