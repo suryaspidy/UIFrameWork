247684f0534c32cc50755f2cbf70bc3dfed96b07
suryaspidy <ss0972203@gmail.com>
2021-07-22 15:11:34 +0530

    commit one

---
+++ UIFrameWork/UIViewDesinger.swift
@@ -0,0 +1,14 @@
+//
+//  UIViewDesinger.swift
+//  UIFrameWork
+//
+//  Created by surya-zstk231 on 22/07/21.
+//
+
+import UIKit
+
+class UIViewDesinger: UIView {
+    func changeColour(){
+        self.backgroundColor = .red
+    }
+}
